985530e3d58168374fc27ecd8cc1c03d318036f4
Vikram Naik <26vikramnaik@gmail.com>
2023-11-08 00:43:54 +0530

    bottom nav bar

--- lib/pages/main_home_page.dart
+++ lib/pages/main_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:health_app/pages/home_page.dart';
 import 'package:health_app/widgets/header_with_profile.dart';
 import 'package:health_app/widgets/health_card.dart';
@@ -13,105 +14,140 @@ class MainPage extends StatelessWidget {
       child: Scaffold(
         appBar: appBar(),
         body: mainHomeBody(size),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedFontSize: 0,
+          unselectedFontSize: 0,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/Home.svg'),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/Frame 1.svg'),
+              label: 'Frame',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/navigate.svg'),
+              label: 'Frame',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/person.svg'),
+              label: 'Profile',
+            ),
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          tooltip: 'Increment',
+          child: SvgPicture.asset(
+            'assets/icons/addIcon.svg',
+          ),
+        ),
       ),
     );
   }
 
   SingleChildScrollView mainHomeBody(Size size) {
     return SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            HeaderWithProfile(size: size),
-            const SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      HealthCard(
-                        size: size,
-                        title: 'Heart',
-                        textColor: Colors.white,
-                        iconPath: 'assets/icons/duotone.svg',
-                        readings: '105',
-                        unit: 'bpm',
-                        borderColor: Colors.black,
-                        backgroundColor: const Color(0xff5142AB),
-                        mainIconPath: 'assets/icons/vector.svg',
-                        // mainIconWidth: size.width * .4,
-                      ),
-                      HealthCard(
-                        size: size,
-                        title: 'Water',
-                        iconPath: 'assets/icons/Water.svg',
-                        mainIconPath: 'assets/icons/water iluss.svg',
-                        readings: '1.0',
-                        unit: 'liters',
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          HealthCard(
-                            size: size,
-                            title: 'Walk',
-                            iconPath: 'assets/icons/walk.svg',
-                            mainIconPath: 'assets/icons/Graph.svg',
-                            height: size.height * .25,
-                            isReadingsCenter: true,
-                            readings: '5460',
-                            unit: 'steps',
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          HealthCard(
-                            size: size,
-                            title: 'Sleep',
-                            iconPath: 'assets/icons/Bed.svg',
-                            height: size.height * .15,
-                            readings: '06:32',
-                            unit: 'hours',
-                          ),
-                        ],
-                      ),
-                      HealthCard(
-                        size: size,
-                        title: 'Calories',
-                        iconPath: 'assets/icons/Calories.svg',
-                        height: size.height * 0.40 + 20,
-                        mainIconPath: 'assets/icons/Line.svg',
-                        readings: '540',
-                        unit: 'kcal',
-                        linearGradient: const LinearGradient(
-                          begin:
-                              Alignment.topCenter, // Starting from the left
-                          end: Alignment.bottomCenter, // Ending at the right
-                          colors: [
-                            Colors.white,
-                            Color.fromARGB(255, 229, 233, 248),
-                          ], // Color stops
-                          stops: [0.0, 1.0819], // Color stops positions
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: [
+          HeaderWithProfile(size: size),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    HealthCard(
+                      size: size,
+                      title: 'Heart',
+                      textColor: Colors.white,
+                      iconPath: 'assets/icons/duotone.svg',
+                      readings: '105',
+                      unit: 'bpm',
+                      borderColor: Colors.black,
+                      backgroundColor: const Color(0xff5142AB),
+                      mainIconPath: 'assets/icons/vector.svg',
+                      // mainIconWidth: size.width * .4,
+                    ),
+                    HealthCard(
+                      size: size,
+                      title: 'Water',
+                      iconPath: 'assets/icons/Water.svg',
+                      mainIconPath: 'assets/icons/water iluss.svg',
+                      readings: '1.0',
+                      unit: 'liters',
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        HealthCard(
+                          size: size,
+                          title: 'Walk',
+                          iconPath: 'assets/icons/walk.svg',
+                          mainIconPath: 'assets/icons/Graph.svg',
+                          height: size.height * .25,
+                          isReadingsCenter: true,
+                          readings: '5460',
+                          unit: 'steps',
                         ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        HealthCard(
+                          size: size,
+                          title: 'Sleep',
+                          iconPath: 'assets/icons/Bed.svg',
+                          height: size.height * .15,
+                          readings: '06:32',
+                          unit: 'hours',
+                        ),
+                      ],
+                    ),
+                    HealthCard(
+                      size: size,
+                      title: 'Calories',
+                      iconPath: 'assets/icons/Calories.svg',
+                      height: size.height * 0.40 + 20,
+                      mainIconPath: 'assets/icons/Line.svg',
+                      readings: '540',
+                      unit: 'kcal',
+                      linearGradient: const LinearGradient(
+                        begin: Alignment.topCenter, // Starting from the left
+                        end: Alignment.bottomCenter, // Ending at the right
+                        colors: [
+                          Colors.white,
+                          Color.fromARGB(255, 229, 233, 248),
+                        ], // Color stops
+                        stops: [0.0, 1.0819], // Color stops positions
                       ),
-                    ],
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      );
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
